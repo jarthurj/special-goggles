5e863cb6676b2a65623b8bdee2cfbf4c42eb61a3
jarthurj <jaoldaker1@gmail.com>
2023-08-03 20:30:30 -0400

    form and sessions

--- former/apps/first_app/views.py
+++ former/apps/first_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-	return HttpResponse("BUTT")
+	return render(request, "first_app/index.html")
+
+def info(request):
+	context = {
+		"name":request.POST['name'],
+		"location":request.POST['loc'],
+		"comments":request.POST['comment'],
+
+	}
+	return render(request, "first_app/info.html", context)
