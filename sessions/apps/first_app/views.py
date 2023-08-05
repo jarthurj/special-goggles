c4da0f11dbb0e4603988815938c0d9e0f22f52ef
jarthurj <jaoldaker1@gmail.com>
2023-08-04 21:13:41 -0400

    comments

--- sessions/apps/first_app/views.py
+++ sessions/apps/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from time import time, strftime, gmtime
 def index(request):
+	request.session.flush()
 	return render(request, 'first_app/index.html')
 
 
@@ -10,13 +11,18 @@ def add(request):
 	temp_dict['color'] = request.POST['color']
 	try:
 		temp_dict['big_font'] = request.POST['big_font']
-			
+		temp_dict['big_font'] = True
+
 	except:
-		temp_dict['big-font'] = 0
+		temp_dict['big-font'] = False
+
+	try:
+		temp_list = request.session['word_list']
+	except:
+		temp_list = []
 	temp_dict['time'] = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-	temp_list = []
+
 	temp_list.append(temp_dict)
-	request.session['word_list'] = temp_list
 	context = {
 		'word_list': temp_list,
 		'ass': "ass", 
